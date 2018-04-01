a8bea0baf90f0989cfb1d7dbf29cccf6e0fe4d5a
dimireme <a_s_d@mail.ru>
2018-04-01 16:14:28 +0500

    Configure babel and webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
+const path = require('path');
+
 module.exports = {
-	entry: './app/main',
+	entry: path.join(__dirname, 'src', 'client', 'app', 'index'),
 	output: {
-		filename: 'build.js'
-	}
+		path: path.join(__dirname, 'src', 'client', 'public'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/,
+				include: path.join(__dirname, 'src', 'client', 'app'),
+				use: 'babel-loader'
+			}
+		]
+	},
+	mode: 'development'
 };
