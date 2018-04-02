17464f1daef5c535987a9b6fdaf3edc111378391
dimireme <a_s_d@mail.ru>
2018-04-03 03:57:09 +0500

    Added comments on the configuration file for the future.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
 
 module.exports = {
+
+	// Точка входа приложения
 	entry: path.join(__dirname, 'src', 'client', 'app', 'index'),
+
+	// Результат сборки
 	output: {
 		path: path.join(__dirname, 'src', 'client', 'public'),
 		filename: 'bundle.js'
 	},
+
+	// Какие расширения файлов могут подключаться в проекте через import
 	resolve: {
 		extensions: ['.js', '.jsx']
 	},
+
+	// Правила сборки модулей
+	// Для файлов с шаблоном {test} внутри директории {include} использовать {use}
+	// Babel-loader помогает сборщику проекта читать файлы jsx, чтобы потом получить итоговый js файл
 	module: {
 		rules: [
 			{
@@ -18,5 +28,7 @@ module.exports = {
 			}
 		]
 	},
+
+	// enable defaults for development environment
 	mode: 'development'
 };
