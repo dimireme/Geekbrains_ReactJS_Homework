aa19002415c0d3e45338e2485b4289197f19e258
dimireme <a_s_d@mail.ru>
2018-04-03 03:52:53 +0500

    Create components Login, Main and Menu.

--- src/client/app/components/main.jsx
+++ src/client/app/components/main.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
-export default class MainContent extends Component {
+export default class Main extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			content: props.content
+		}
+	}
+
 	render() {
-		<main>
-			<p>This is main text</p>
-		</main>
+		const { content } = this.state;
+
+		return (
+			<main>
+				<h2>This is main section</h2>
+				{ content }
+			</main>
+		);
 	}
 }
