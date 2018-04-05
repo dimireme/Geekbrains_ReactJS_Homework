57307dd00d9558c46d31c69e77b45e7a680e21e2
dimireme <a_s_d@mail.ru>
2018-04-05 20:08:26 +0500

    The structure of the application is compiled.

---
+++ src/client/app/components/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { Button } from 'reactstrap';
+
+export default class Main extends Component {
+	render() {
+		return (
+			<main>
+
+			</main>
+		);
+	}
+}
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 class App extends Component {
 	render() {
-
+		return (
+			<div className="container">
+					<Header />
+					<Main />
+					<Footer color="dark"/>
+			</div>
+		);
 	}
 }
 
